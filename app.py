02abf84b75437766b0302ed5d21cd91b7de76b78
ameliarider <mimirider89@gmail.com>
2025-07-21 09:41:06 -0700

    Add index action

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+import db
 
 app = Flask(__name__)
 cors = CORS(app, resources={"/*": {"origins": "http://localhost:5173", "supports_credentials": True}})
@@ -8,3 +9,7 @@ cors = CORS(app, resources={"/*": {"origins": "http://localhost:5173", "supports
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/posts.json")
+def index():
+    return db.posts_all()
